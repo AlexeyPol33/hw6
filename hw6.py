32a0cf5e5c7f7a184c70fba7a1f31b775462f458
Alexey Poltavskiy <79776177342@yandex.ru>
2022-09-16 16:19:34 +0300

    third task done

--- hw6.py
+++ hw6.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -21,6 +24,25 @@ class Student:
                 lecturer.grades[course] = [grade]
         else:
             return 'Ошибка'
+    
+    def midle_rate (self):
+        onelist_rate = reduce(lambda x,y: x + y, list(self.grades.values()))
+        sum_rate = reduce(lambda x,y: x + y, onelist_rate)
+        return round(sum_rate / len(onelist_rate),1)
+
+    def __str__(self) -> str:
+
+
+        return 'Имя: ' + self.name + '\n' + 'Фамилия: ' + self.surname + '\n' + f'Средняя оценка за домашние задания: {self.midle_rate ()}' + '\n' + 'Курсы в процессе изучения: ' + ', '.join(self.courses_in_progress) + '\n' + 'Завершенные курсы: ' + ', '.join(self.finished_courses)
+
+    def __lt__ (self, other):
+
+        return self.midle_rate() < other.midle_rate()
+    
+    def __gt__ (self, other):
+
+        return self.midle_rate() > other.midle_rate()
+
         
 class Mentor:
     def __init__(self, name, surname):
@@ -34,6 +56,16 @@ class Lecturer (Mentor):
         super().__init__(name, surname)
         self.grades = {}
 
+    def midle_rate (self):
+        onelist_rate = reduce(lambda x,y: x + y, list(self.grades.values()))
+        sum_rate = reduce(lambda x,y: x + y, onelist_rate)
+        return round(sum_rate / len(onelist_rate),1)
+
+    def __str__(self) -> str:
+
+
+        return 'Имя: ' + self.name + '\n' + 'Фамилия: ' + self.surname + '\n' + f'Средняя оценка за лекции: {self.midle_rate()}' 
+
 
 class Reviewer (Mentor):
     def rate_hw(self, student, course, grade):
@@ -45,8 +77,13 @@ class Reviewer (Mentor):
         else:
             return 'Ошибка'
 
+    def __str__(self) -> str:
+        return 'Имя: ' + self.name + '\n' + 'Фамилия: ' + self.surname
+
+
 best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
+best_student.courses_in_progress += ['Python','GIT']
+best_student.finished_courses += ['Введение в программирование']
  
 cool_mentor = Mentor('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
@@ -57,13 +94,24 @@ cool_lecturer.courses_attached += ['Python']
 cool_revier = Reviewer('Some', 'Buddy')
 cool_revier.courses_attached += ['Python']
 
-cool_revier.rate_hw(best_student, 'Python', 10)
-cool_revier.rate_hw(best_student, 'Python', 10)
-cool_revier.rate_hw(best_student, 'Python', 0)
+cool_revier.rate_hw(best_student, 'Python', 7)
+cool_revier.rate_hw(best_student, 'Python', 3)
+cool_revier.rate_hw(best_student, 'Python', 1)
 
 best_student.rate_lecturer (cool_lecturer, 'Python', 10)
 best_student.rate_lecturer (cool_lecturer, 'Python', 10)
 best_student.rate_lecturer (cool_lecturer, 'Python', 10)
  
 print(best_student.grades)
+print()
 print(cool_lecturer.grades)
+print()
+print (cool_revier)
+print()
+print(cool_lecturer)
+print()
+print(best_student)
+print()
+print(best_student < cool_lecturer)
+print()
+print(best_student > cool_lecturer)
